4eebd6522b7938adc3bdf8fff5632f926ae39134
Ryan Dao <daoduyducduong@gmail.com>
2014-08-17 11:55:55 +0700

    Make sure the console is only rendered once
    If both controller and view helpers are present, the view helper takes
    precedence.

--- lib/web_console/view_helpers.rb
+++ lib/web_console/view_helpers.rb
@@ -1,19 +1,16 @@
 module WebConsole
   module ViewHelpers
     def console(console_binding = nil)
-      # This makes sure the console is only rendered once in a template
-      @_should_render_console = true if @_should_render_console.nil?
-
       if WebConsole.binding_of_caller_available?
         console_binding ||= binding.callers[1]
       end
 
-      if @_should_render_console
+      if controller.should_render_console
         @console_session = WebConsole::REPLSession.create(
           binding: console_binding
         )
 
-        @_should_render_console = false
+        controller.should_render_console = false
         render('rescues/web_console')
       end
     end
