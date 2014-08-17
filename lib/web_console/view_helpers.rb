dabe601aca834fd64fd731d3befd347dd15c6e76
Ryan Dao <daoduyducduong@gmail.com>
2014-08-17 12:22:45 +0700

    Use binding.of_caller instead of binding.callers

--- lib/web_console/view_helpers.rb
+++ lib/web_console/view_helpers.rb
@@ -2,7 +2,7 @@ module WebConsole
   module ViewHelpers
     def console(console_binding = nil)
       if WebConsole.binding_of_caller_available?
-        console_binding ||= binding.callers[1]
+        console_binding ||= binding.of_caller(1)
       end
 
       if controller.should_render_console
